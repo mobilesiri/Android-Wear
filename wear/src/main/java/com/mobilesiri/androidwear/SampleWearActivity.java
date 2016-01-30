989da25f1abc0e747ec8fc52698602bee62c1e60
MBA <muhammadbilalansari@yahoo.com>
2016-01-30 20:46:19 +0500

    Commit for Github

---
+++ wear/src/main/java/com/mobilesiri/androidwear/SampleWearActivity.java
@@ -0,0 +1,25 @@
+package com.mobilesiri.androidwear;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class SampleWearActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_wear);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+                mTextView.setText("Hi from Mobilesiri.com");
+            }
+        });
+    }
+}
